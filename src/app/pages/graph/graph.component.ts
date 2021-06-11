07401f50bc0f37fff43fc8c3b4cf2fdec164844c
Rodrigo Moraga <roro.moraga@gmail.com>
2021-06-10 20:28:34 -0400

    first commit

---
+++ src/app/pages/graph/graph.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graph',
+  template: `
+    <p>
+      graph works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class GraphComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
